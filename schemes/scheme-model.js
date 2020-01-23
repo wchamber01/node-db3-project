dae86e3be080e4e6a01f2835fb7bce23d6337add
wchamber01 <williamchambers@gmail.com>
2020-01-22 21:46:54 -0500

    working on scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,42 @@
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("schemes").where({ id });
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where("id", id)
+    .del();
+}
